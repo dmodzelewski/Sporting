13b183efbb8d2daca467535610e163ab951ebe48
dmodzelewski <daniel.modzelewski5@gmail.com>
2020-03-21 12:48:10 +0100

    Add component to app

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+            <h1>iSportio</h1>
+            <p>Najlepsze miejsce by zacząć</p>
+        </div>
+        )
+    }
+}
--- src/app.js
+++ src/app.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
-
+import Home from './Pages/Home'
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>iSportio</h1>
-                <p>Najlepsze miejsce by zacząć</p>
-            </div>
-            
-            
+              <Home/>       
             );
     }
 }
