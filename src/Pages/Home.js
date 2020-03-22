a8d3a9f79f5ce1fd15fc26e558a5d386190bd4ab
dmodzelewski <daniel.modzelewski5@gmail.com>
2020-03-22 16:40:30 +0100

    adding react-bootstrap (need to start compose the website )

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
-
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 export default class Home extends Component {
     render() {
         return (
             <>
-            <div className="defaultHero">
-                <h1>iSportio</h1>
-                <p>Najlepsze miejsce by zacząć</p>
-            </div>
-        </>
+                <Container>
+                    <Row>
+                        <Col><h1>Sportio</h1></Col>
+                    
+                    </Row>
+                    <Row>
+                        <Button variant="outline-warning">Jeden Przycisk</Button>
+                        <Button variant="outline-danger">Jeden Przycisk</Button>
+                        <Button  variant="outline-info"> Jeden Przycisk</Button>
+                    </Row>
+                </Container>
+            </>
         )
     }
 }
--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import Singin from './Functionality/Singin';
 import Login from './Functionality/Login';
 import Navbar from "./Components/Navbar"
 import './app.scss'
-import 'bootstrap';
+
 import { Route, Switch, Router } from "react-router-dom"
 class App extends Component {
     render() {
