abb77025e6141baa225206d77e11bd2cdbff11ab
dmodzelewski <daniel.modzelewski5@gmail.com>
2020-08-08 13:50:41 +0200

    Select localization onClick

--- src/Components/CommonComponents/Search/Localization/Filter.js
+++ src/Components/CommonComponents/Search/Localization/Filter.js
@@ -5,27 +5,35 @@ import PropTypes from "prop-types";
 
 export default function Filter({ cities }) {
   const [city, setcity] = useState("");
+  const SelectCityHandler = (e) => {
+    setcity(e.target.innerHTML.toString());
+  };
+
+  const { loading, error, data } = useQuery(cities, {
+    variables: { localization: city },
+    pollInterval: 500,
+  });
 
   function RenderData() {
-    const { loading, error, data } = useQuery(cities, {
-      variables: { localization: city },
-      pollInterval: 500,
-    });
     if (loading) return <p className="search-filter-city">Loading...</p>;
     if (error) return `Error! ${error.message} `;
     if (city === "") return "";
+
     return (
       <>
         <div className="search-filter-city">
           <ul>
             {data.cities.map(({ NAZWA }) => (
-              <li key={NAZWA}>{NAZWA}</li>
+              <li key={NAZWA} onClick={SelectCityHandler.bind(this)}>
+                {NAZWA}
+              </li>
             ))}
           </ul>
         </div>
       </>
     );
   }
+
   return (
     <>
       <Form.Control
@@ -36,7 +44,6 @@ export default function Filter({ cities }) {
         onChange={(e) => setcity(e.target.value)}
         value={city}
       />
-
       {RenderData()}
     </>
   );
