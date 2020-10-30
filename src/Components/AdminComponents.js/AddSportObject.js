764a77d1130c13f505b8826dec4c8e1d4059d400
dmodzelewski <modzel132@gmail.com>
2020-10-30 16:04:11 +0100

    little change

--- src/Components/AdminComponents.js/AddSportObject.js
+++ src/Components/AdminComponents.js/AddSportObject.js
@@ -1,137 +1,34 @@
 import React from "react";
-import { Button, Form, Col, InputGroup } from "react-bootstrap";
-import { Formik } from "formik";
-import * as yup from "yup";
-
-const schema = yup.object({
-  firstName: yup.string().required(),
-  lastName: yup.string().required(),
-  username: yup.string().required(),
-  city: yup.string().required(),
-  state: yup.string().required(),
-  zip: yup.string().required(),
-  file: yup.string().required(),
-  terms: yup.bool().required(),
-});
-
+import { Button, Form } from "react-bootstrap";
+import ReverseGeocoding from "../CommonComponents/Search/GetCurrentPosition";
 const AddSportObject = () => {
   return (
-    <Formik
-      validationSchema={schema}
-      onSubmit={console.log}
-      initialValues={{
-        firstName: "Jan",
-        lastName: "Kowalski",
-      }}
-    >
-      {({ handleSubmit, handleChange, values, touched, errors }) => (
-        <Form noValidate onSubmit={handleSubmit}>
-          <Form.Row>
-            <Form.Group as={Col} md="4" controlId="validationFormik01">
-              <Form.Label>Nazwa Obiektu</Form.Label>
-              <Form.Control
-                type="text"
-                name="firstName"
-                value={values.firstName}
-                onChange={handleChange}
-                isValid={touched.firstName && !errors.firstName}
-              />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationFormik02">
-              <Form.Label>Last name</Form.Label>
-              <Form.Control
-                type="text"
-                name="lastName"
-                value={values.lastName}
-                onChange={handleChange}
-                isValid={touched.lastName && !errors.lastName}
-              />
-
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationFormikUsername">
-              <Form.Label>Username</Form.Label>
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                </InputGroup.Prepend>
-                <Form.Control
-                  type="text"
-                  placeholder="Username"
-                  aria-describedby="inputGroupPrepend"
-                  name="username"
-                  value={values.username}
-                  onChange={handleChange}
-                  isInvalid={!!errors.username}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors.username}
-                </Form.Control.Feedback>
-              </InputGroup>
-            </Form.Group>
-          </Form.Row>
-          <Form.Row>
-            <Form.Group as={Col} md="6" controlId="validationFormik03">
-              <Form.Label>City</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="City"
-                name="city"
-                value={values.city}
-                onChange={handleChange}
-                isInvalid={!!errors.city}
-              />
-
-              <Form.Control.Feedback type="invalid">
-                {errors.city}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="3" controlId="validationFormik04">
-              <Form.Label>State</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="State"
-                name="state"
-                value={values.state}
-                onChange={handleChange}
-                isInvalid={!!errors.state}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.state}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="3" controlId="validationFormik05">
-              <Form.Label>Zip</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Zip"
-                name="zip"
-                value={values.zip}
-                onChange={handleChange}
-                isInvalid={!!errors.zip}
-              />
-
-              <Form.Control.Feedback type="invalid">
-                {errors.zip}
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Form.Row>
-          <Form.Group>
-            <Form.Check
-              required
-              name="terms"
-              label="Agree to terms and conditions"
-              onChange={handleChange}
-              isInvalid={!!errors.terms}
-              feedback={errors.terms}
-              id="validationFormik0"
-            />
-          </Form.Group>
-          <Button type="submit">Submit form</Button>
-        </Form>
-      )}
-    </Formik>
+    <Form>
+      <Form.Group>
+        <Form.Label>Nazwa Obiektu</Form.Label>
+        <Form.Control type="text" placeholder="Wprowadź nazwę" />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Miasto</Form.Label>
+        <Form.Control type="text" placeholder="Wprowadź miasto" />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Ulice</Form.Label>
+        <Form.Control type="text" placeholder="Wprowadź Ulice" />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Nr. budynku</Form.Label>
+        <Form.Control type="text" placeholder="Wprowadź Nr. budynku" />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Nr.Lokalu</Form.Label>
+        <Form.Control type="text" placeholder="Wprowadź Nr.Lokalu" />
+        <Form.Text>Wprowadź jeżeli istnieje</Form.Text>
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={ReverseGeocoding()}>
+        Submit
+      </Button>
+    </Form>
   );
 };
 export default AddSportObject;
